784a77e22a016f8b79f32db0aab74743341346e1
calibhden <ardyan.yohan@gmail.com>
2026-08-11 17:01:46 +0700

    Clean booking player migration

--- supabase/007_booking_player_editor.sql
+++ supabase/007_booking_player_editor.sql
@@ -1,4 +1,3 @@
-/opt/homebrew/Library/Homebrew/cmd/shellenv.sh: line 18: /bin/ps: Operation not permitted
 create or replace function public.set_booking_players(
   target_booking_id uuid,
   target_player_ids bigint[] default '{}'::bigint[]
